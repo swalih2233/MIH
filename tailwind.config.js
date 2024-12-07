a16d4982c810c99c6bcb96aeebfcfda3bd79ae6a
swalih <swalihkpx@gmail.com>
2024-12-07 17:29:55 +0530

    first-release

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          cardo: ['Cardo', 'serif'], // Add Cardo as a custom font
+        },
+      },
+    },
+    plugins: [],
+  };
+  
